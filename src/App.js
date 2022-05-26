b6852d12d4fd42579806e0badb7796c7a6eebdf5
Mosh2018 <43504065+Mosh2018@users.noreply.github.com>
2022-05-26 17:51:16 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mohamid Shaker
+          Mohamid Shaker 2
         </p>
       </header>
     </div>

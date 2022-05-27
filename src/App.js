6741ee0fadc03ee5d7d17f2bb9ff19d08a018a7a
Mohamid Shaker <70940766+Moha-Sha@users.noreply.github.com>
2022-05-27 14:10:11 +0300

    test changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mohamid Shaker 2
+          Mohamid Shaker:n websivu toimii
         </p>
       </header>
     </div>
